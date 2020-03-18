5d4e0af541f53b72204bdd82ce5b8fe28bc028b5
Ivo Lima Silva <ivolimasilva@outlook.com>
2020-03-18 18:34:53 +0000

    fix: smooth scroll on ios

--- src/shared/layouts/Layout.js
+++ src/shared/layouts/Layout.js
@@ -1,21 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import smoothscroll from 'smoothscroll-polyfill';
 
 import { Header, Footer, SEO } from '../components';
 
 import '../styles/global/index.css';
 import styles from './Layout.module.css';
 
-export const Layout = ({ children }) => (
-    <Fragment>
-        <SEO />
-        <Header />
-        <main className={ styles.main }>
-            { children }
-        </main>
-        <Footer />
-    </Fragment>
-);
+export const Layout = ({ children }) => {
+    useEffect(() => {
+        smoothscroll.polyfill();
+    }, []);
+
+    return (
+        <Fragment>
+            <SEO />
+            <Header />
+            <main className={ styles.main }>
+                { children }
+            </main>
+            <Footer />
+        </Fragment>
+    );
+};
 
 Layout.propTypes = {
     children: PropTypes.node.isRequired,
