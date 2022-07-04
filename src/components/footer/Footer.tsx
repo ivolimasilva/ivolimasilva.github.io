373be2681428b5dd43093a4e3d0bbcdc548dfe1b
Ivo Lima Silva <ivolimasilva@outlook.com>
2022-07-04 14:01:46 +0100

    refactor: spacings 📏

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,23 +1,12 @@
-import { useCallback } from 'react';
-
 import RocketIcon from '../rocket-icon';
 
 import styles from './Footer.module.css';
 
-export const Footer = () => {
-	const handleClick = useCallback(() => {
-		window.scrollTo({
-			top: 0,
-			behavior: 'smooth',
-		});
-	}, []);
-
-	return (
-		<footer className={ styles.container }>
-			<p>Thanks for visiting</p>
-			<RocketIcon onClick={ handleClick } className={ styles.rocket } />
-		</footer>
-	);
-};
+export const Footer = () => (
+	<footer className={ styles.container }>
+		<p>Thanks for visiting</p>
+		<RocketIcon className={ styles.rocket } />
+	</footer>
+);
 
 export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
-import { Fragment, useEffect } from 'react';
+import { Fragment } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import smoothscroll from 'smoothscroll-polyfill';
 
 import { Header, Footer } from '../components';
 import Favicon from '../media/favicons/favicon.ico';
@@ -12,26 +11,20 @@ import Icon32 from '../media/favicons/favicon-32x32.png';
 
 import '../styles/global/index.css';
 
-const App = ({ Component, pageProps }: AppProps) => {
-	useEffect(() => {
-		smoothscroll.polyfill();
-	}, []);
-
-	return (
-		<Fragment>
-			<Head>
-				<title>Ivo Lima Silva • Frontend Developer <span aria-hidden>💻</span></title>
-				<link rel="icon" href={ Favicon.src } type="image/x-icon" />
-				<link rel="apple-touch-icon" href={ IconApple.src } sizes="180x180" />
-				<link rel="icon" type="image/png" href={ Icon16.src } sizes="16x16" />
-				<link rel="icon" type="image/png" href={ Icon32.src } sizes="32x32" />
-				<link rel="mask-icon" href={ IconSafariPinned.src } />
-			</Head>
-			<Header />
-			<Component { ...pageProps } />
-			<Footer />
-		</Fragment>
-	);
-};
+const App = ({ Component, pageProps }: AppProps) => (
+	<Fragment>
+		<Head>
+			<title>Ivo Lima Silva • Frontend Developer 💻</title> { /* eslint-disable-line jsx-a11y/accessible-emoji */ }
+			<link rel="icon" href={ Favicon.src } type="image/x-icon" />
+			<link rel="apple-touch-icon" href={ IconApple.src } sizes="180x180" />
+			<link rel="icon" type="image/png" href={ Icon16.src } sizes="16x16" />
+			<link rel="icon" type="image/png" href={ Icon32.src } sizes="32x32" />
+			<link rel="mask-icon" href={ IconSafariPinned.src } />
+		</Head>
+		<Header />
+		<Component { ...pageProps } />
+		<Footer />
+	</Fragment>
+);
 
 export default App;
