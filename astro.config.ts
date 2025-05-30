7b6c742a313d017fb1c42206affa877ea27dcb91
Ivo Lima Silva <ivolimasilva@outlook.com>
2025-05-30 18:22:47 +0100

    chore: update dependencies & use `server mode` 👷

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	output: 'server',
+	adapter: vercel(),
 });
